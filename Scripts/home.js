6b1fd0367c98ab567eb6ee40c3afc4e917c1ec2b
EchoSierraGit <104131337+EchoSierraGit@users.noreply.github.com>
2024-04-22 20:37:23 +0200

    quick adjustments

---
+++ Scripts/home.js
@@ -0,0 +1,11 @@
+function MoveSidebar(){
+    var e = document.getElementById('Sidebar');
+
+    e.style.right = '0px';
+}
+
+function CloseSidebar(){
+    var e = document.getElementById('Sidebar');
+
+    e.style.right = '-240px';
+}
