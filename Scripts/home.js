50b98fdb56718babef6e96b02ec6b712b409a139
EchoSierraGit <104131337+EchoSierraGit@users.noreply.github.com>
2024-05-05 11:59:50 +0200

    wip

--- Scripts/home.js
+++ Scripts/home.js
@@ -10,3 +10,25 @@ function CloseSidebar(){
     e.style.right = '-240px';
 }
 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.scroll-link').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if(targetElement) {
+                // Scroll to the top of the target element
+                window.scrollTo({
+                    top: targetElement.offsetTop - 100,
+                    behavior: 'smooth'
+                });
+            }
+
+           
+        });
+    });
+});
